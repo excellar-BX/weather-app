7ebd033fa006351e3f1f7c82d7238c8392390bae
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-02-07 20:04:04 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import "./globals.css";
 
 
@@ -12,12 +11,6 @@ export default function RootLayout({
       <body
         className={` antialiased`}
       >
-        <Head>
-          <title>Weather app</title>
-      <link rel="icon" type="image/png" href='/public/partly-cloudy' />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Check live weather updates!" />
-    </Head>
         <main>
         {children}
         </main>
