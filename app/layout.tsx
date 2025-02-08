055d8fb2d58b5ac4c5102a708be62029374482c9
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-02-07 20:16:56 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,13 @@
+import type { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Weather App",
+  description: "A tool for checking real-time weather conditions in various cities",
+  icons: {
+    icon: "/partly-cloudy.png", // This should be inside `public/`
+  },
+};
 
 
 export default function RootLayout({
